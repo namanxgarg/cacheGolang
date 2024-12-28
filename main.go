518406806f2bc6336b642317534ce32d3eb1224c
namanxgarg <social.namangarg@gmail.com>
2024-12-28 16:22:34 +0530

    built a working model

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const SIZE = 5
+
 type Node struct {
 	Val   string
 	Left  *Node
@@ -24,41 +26,67 @@ func NewCache() Cache {
 }
 
 func newQueue() Queue {
-	head:=&Node{}
-	tail:=&Node{}
-	head.Right=tail
-	tail.Left=head
-	return Queue{Head: head, Tail: tail, Length}
+	head := &Node{}
+	tail := &Node{}
+	head.Right = tail
+	tail.Left = head
+	return Queue{Head: head, Tail: tail}
 }
 
-func (c *Cache) Check(str string){
-	node:=&Node{}
-	
-	if val, ok:=c.Hash[str]; ok{
-		node= c.Remove(val)
-	}else{
-		node=&Node{Val: str}
+func (c *Cache) Check(str string) {
+	node := &Node{}
+
+	if val, ok := c.Hash[str]; ok {
+		node = c.Remove(val)
+	} else {
+		node = &Node{Val: str}
 	}
 	c.Add(node)
 }
 
-func (c *Cache) Remove(n *Node) *Node{
+func (c *Cache) Remove(n *Node) *Node {
 	fmt.Printf("remove: %s\n", n.Val)
-	left:=n.Left
-	right:=n.Right
-	right.Left=left
-	left.Right=right
-	c.Queue.Length-=1;
+	left := n.Left
+	right := n.Right
+	right.Left = left
+	left.Right = right
+	c.Queue.Length -= 1
 	delete(c.Hash, n.Val)
 	return n
 }
 
-func (c *Cache) Add(n *Node) *Node{
+func (c *Cache) Add(n *Node) *Node {
 	fmt.Printf("add %s\n", n.Val)
-	tmp:=c.Queue.Head.Right
+	tmp := c.Queue.Head.Right
 
-	c.Queue.Head.Right=n
-	
+	c.Queue.Head.Right = n
+	n.Left = c.Queue.Head
+	n.Right = tmp
+	tmp.Left = n
+
+	c.Queue.Length++
+
+	if c.Queue.Length > SIZE {
+		c.Remove(c.Queue.Tail.Left)
+	}
+	return n
+}
+
+func (c *Cache) Display() {
+	c.Queue.Display()
+}
+
+func (q *Queue) Display() {
+	node := q.Head.Right
+	fmt.Printf("%d - [", q.Length)
+	for i := 0; i < q.Length; i++ {
+		fmt.Printf("{%s}", node.Val)
+		if i < q.Length-1 {
+			fmt.Printf("<-->")
+		}
+		node = node.Right
+	}
+	fmt.Printf("]")
 }
 
 type Hash map[string]*Node
